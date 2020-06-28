c73b4070a925b241b4c68ad6af57cbbdb339c563
Avinash Kumar <avinash.kumar@walmartlabs.com>
2020-06-28 13:56:45 +0530

    changes for portfolio

---
+++ precache-manifest.332d56ed31fee37c8e1953e7351c7f88.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82493243b0844469842253af70353831",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bb36ccd9a09575b6fb97",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "edb9490b5b04ef067eb5",
+    "url": "/static/js/2.873ef92e.chunk.js"
+  },
+  {
+    "revision": "afcffdf4ebf1780e0970711996cdd7dd",
+    "url": "/static/js/2.873ef92e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb36ccd9a09575b6fb97",
+    "url": "/static/js/main.d67dc5f5.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "fdf6b2d975fc75a00e52b782ff7afaf1",
+    "url": "/static/media/My_portfolio_pic.fdf6b2d9.jpeg"
+  },
+  {
+    "revision": "532b8001988442a797fa3e9217ec308e",
+    "url": "/static/media/favicon.532b8001.jpeg"
+  },
+  {
+    "revision": "fde665ea611f4512ed36f6bfc46ccf56",
+    "url": "/static/media/me.fde665ea.jpeg"
+  },
+  {
+    "revision": "011bf697a58d2990abedb7022fea4ac5",
+    "url": "/static/media/ms_logo.011bf697.png"
+  },
+  {
+    "revision": "e2d3f579f2640b633fe77859affaad9f",
+    "url": "/static/media/ps-logo.e2d3f579.svg"
+  },
+  {
+    "revision": "0b1d9c2e640356c043ab0662268d61ae",
+    "url": "/static/media/walmart-logo.0b1d9c2e.png"
+  }
+]);
